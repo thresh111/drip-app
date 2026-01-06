0886552db66435d07bb8828decbc281d9d4d6a75
thresh <thresh847@163.com>
2026-01-06 15:02:36 +0800

    refactor(login): simplify login page layout by removing sign-in options and input fields; update structure for improved readability

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,4 @@
 import SvgIcon from '@/components/common/svg-icon';
-import { Button, Divider, Input } from 'antd';
 
 function Login() {
   return (
@@ -8,22 +7,7 @@ function Login() {
         <SvgIcon name={'manus-logo'} className={'size-7'} />
         <span className={'text-xl font-bold'}>Drip Finger</span>
       </div>
-      <div className={'flex h-full w-full flex-1 flex-col items-center justify-center gap-2'}>
-        <SvgIcon name={'manus-logo'} className={'size-14'} />
-        <div className={'text-2xl font-bold'}>登录或注册</div>
-        <div className={'text-sm text-(--text-secondary)'}>和 Manus 一起开始创作</div>
-        <div className={'flex w-[360px] flex-col gap-2'}>
-          <Button block> 使用 Google 登录</Button>
-          <Button block>使用 Apple 登录</Button>
-
-          <Divider>
-            <span className={'text-sm'}>或者</span>
-          </Divider>
-
-          <Input placeholder={'请输入您的邮箱地址'} />
-          <Button type={'primary'}>登录</Button>
-        </div>
-      </div>
+      <div className={'flex h-full w-full flex-1 flex-col items-center justify-center gap-2'}>123123</div>
     </div>
   );
 }
