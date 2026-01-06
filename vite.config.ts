e36e27d3f3e276f8b3a1c8670b0aec89c91057c0
thresh <thresh847@163.com>
2026-01-06 11:09:32 +0800

    feat(dependencies, styling): replace @ant-design/icons with @douyinfe/semi-ui-19; add new semi-theme variables and integrate @jacob-z/vite-plugin-semi-theme-loader for enhanced theming support

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'node:path';
 import svgr from 'vite-plugin-svgr';
+import SemiPlugin from '@jacob-z/vite-plugin-semi-theme-loader';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    SemiPlugin({
+      theme: '@semi-bot/semi-theme-drip',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
