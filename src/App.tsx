ad2edbeb8999c9804711bc1aa2a17a0b9c8e9aca
thresh <thresh847@163.com>
2026-01-06 11:12:58 +0800

    refactor(App): remove ConfigProvider to simplify component structure and improve readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { ConfigProvider } from 'antd';
-import zhCN from 'antd/locale/zh_CN';
 import { RouterProvider } from 'react-router';
 import { HashModalRoot, registerHashModal } from '@/components/common/HashModal';
 import { SearchModal } from '@/components/common/HashModal/modals/SearchModal';
@@ -9,29 +7,10 @@ registerHashModal('search', SearchModal);
 
 function App() {
   return (
-    <ConfigProvider
-      modal={{
-        mask: {
-          blur: false,
-        },
-      }}
-      drawer={{
-        mask: {
-          blur: false,
-        },
-      }}
-      tag={{
-        styles: {
-          root: {
-            marginInlineEnd: 8,
-          },
-        },
-      }}
-      locale={zhCN}
-    >
+    <>
       <RouterProvider router={routes} />
       <HashModalRoot />
-    </ConfigProvider>
+    </>
   );
 }
 
